167b05bf8a732bac32aa9ffe92e8a7b47be78c3f
kennyhuang <kennyhuang9@berkeley.edu>
2019-09-29 23:28:20 -0700

    finished backend db construction with all/add/insert functionalities

---
+++ hashwolf2/src/main/java/com/HashWolf2/hashwolf2/DataBaseSeeder.java
@@ -0,0 +1,34 @@
+package com.HashWolf2.hashwolf2;
+
+import com.HashWolf2.hashwolf2.Model.User;
+import com.HashWolf2.hashwolf2.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
+public class DataBaseSeeder implements CommandLineRunner {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public DataBaseSeeder(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String ... strings) throws Exception {
+
+        List<User> list = new ArrayList<>();
+
+        //list.add(new User(10, "Kenny", "kennyhuang9@qq.com"));
+        //list.add(new User(20, "Zichuan", "zeichuan@berkeley.edu"));
+
+        userRepository.saveAll(list);
+
+    }
+}
---
+++ hashwolf2/src/main/java/com/HashWolf2/hashwolf2/Hashwolf2Application.java
@@ -0,0 +1,13 @@
+package com.HashWolf2.hashwolf2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hashwolf2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hashwolf2Application.class, args);
+	}
+
+}
